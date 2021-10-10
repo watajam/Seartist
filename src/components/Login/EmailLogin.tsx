0b50a1ffeb0689d37d74220992369f3a35b3060f
wataru <wata124coron@icloud.com>
2021-10-10 17:50:41 +0900

    ✨ ログイン機能の追加

--- src/components/Login/EmailLogin.tsx
+++ src/components/Login/EmailLogin.tsx
@@ -1,10 +1,12 @@
 import React, { memo, useCallback, useState } from "react";
 import { useForm } from "react-hook-form";
 import Link from "next/link";
+import { signInWithEmailAndPassword } from "@firebase/auth";
+import { auth } from "../../../lib/firebase";
 
 type LoginFormData = {
-  email: string | number;
-  password: number;
+  email: string;
+  password: string;
 };
 
 type Props = {
@@ -13,15 +15,48 @@ type Props = {
 };
 
 const MailLogin: React.VFC<Props> = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm<LoginFormData>();
 
-  const onSubmit = (data: LoginFormData) => {
-    console.log(data);
-  };
+  const onSubmit = useCallback(
+    (data: LoginFormData) => {
+      setEmail(data.email);
+      setPassword(data.password);
+    },
+    [email, password]
+  );
+
+  const handeleChengeEmail = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setEmail(e.target.value);
+    },
+    []
+  );
+
+  const handeleChengePassword = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setPassword(e.target.value);
+    },
+    []
+  );
+
+  //ログイン機能
+  const login = useCallback(
+    async (e: React.MouseEvent<HTMLButtonElement>) => {
+      try {
+        await signInWithEmailAndPassword(auth, email, password);
+        <Link href="/"></Link>;
+      } catch (error) {
+        alert("ログインできませんでした");
+      }
+    },
+    [auth, email, password]
+  );
 
   return (
     <>
@@ -31,8 +66,8 @@ const MailLogin: React.VFC<Props> = (props) => {
         <label className="block mt-8 text-base text-gray-400 ">
           メールアドレス
           <input
-            placeholder="メールアドレスを入力してください"
             type="email"
+            placeholder="メールアドレスを入力してください"
             {...register("email", {
               required: "必須項目です。",
               pattern: {
@@ -41,7 +76,8 @@ const MailLogin: React.VFC<Props> = (props) => {
                 message: "正しい形式で入力してください",
               },
             })}
-            className="w-full h-10 pl-2 mt-2 text-base border border-orange-400 cursor-pointer focus:outline-none focus:ring focus:border-blue-300"
+            className="w-full h-10 pl-2 mt-2 text-base text-black border border-orange-400 cursor-pointer focus:outline-none focus:ring focus:border-blue-300"
+            onChange={handeleChengeEmail}
           />
         </label>
         {errors.email && (
@@ -51,7 +87,6 @@ const MailLogin: React.VFC<Props> = (props) => {
         <label className="block mt-8 text-base text-gray-400 ">
           パスワード
           <input
-            type="password"
             placeholder="パスワードを入力してください"
             {...register("password", {
               required: "必須項目です。",
@@ -64,20 +99,25 @@ const MailLogin: React.VFC<Props> = (props) => {
                 message: "パスワードは6文字数以上16文字以下で入力してください",
               },
             })}
-            className="w-full h-10 pl-2 mt-2 text-base border border-orange-400 cursor-pointer focus:outline-none focus:ring focus:border-blue-300"
+            className="w-full h-10 pl-2 mt-2 text-base text-black border border-orange-400 cursor-pointer focus:outline-none focus:ring focus:border-blue-300"
+            type="password"
+            onChange={handeleChengePassword}
           />
         </label>
         {errors.password && (
           <p className="text-red-600"> {errors.password?.message} </p>
         )}
         <div className="mt-4 text-right">
-          <Link href="">
+          <Link href="/">
             <a className="inline-block text-base text-gray-400 underline cursor-pointer hover:text-gray-600 ">
               {props.isLogin ? "パスワードを忘れた場合" : null}
             </a>
           </Link>
         </div>
-        <button className="w-full py-3 mt-10 text-2xl font-bold text-white bg-orange-300 border rounded-xl hover:bg-orange-400">
+        <button
+          onClick={login}
+          className="w-full py-3 mt-10 text-2xl font-bold text-white bg-orange-300 border rounded-xl hover:bg-orange-400"
+        >
           {props.isLogin ? "ログイン" : "アカウント作成"}
         </button>
       </form>
