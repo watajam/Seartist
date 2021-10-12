42f59d39343addbf7c85a76637f50e7bf4df95a7
wataru <wata124coron@icloud.com>
2021-10-12 23:23:40 +0900

    ♻️ 必要のない記載を削除

--- src/components/Login/GuestLogin.tsx
+++ src/components/Login/GuestLogin.tsx
@@ -3,13 +3,11 @@ import Link from "next/link";
 
 const GuestLogin = () => {
   return (
-    <>
-      <Link href="/">
-        <a className="border block text-center w-full  mt-6  py-3 font-bold  text-2xl text-white rounded-xl bg-gray-400 hover:bg-gray-500">
-          ゲストログイン
-        </a>
-      </Link>
-    </>
+    <Link href="/">
+      <a className="border block text-center w-full  mt-6  py-3 font-bold  text-2xl text-white rounded-xl bg-gray-400 hover:bg-gray-500">
+        ゲストログイン
+      </a>
+    </Link>
   );
 };
 
