1ed686c61e0b15bb0de1bcd2f4ed4c9fc0580213
wataru <wata124coron@icloud.com>
2021-10-12 23:21:58 +0900

    💄 スタイルとレイアウトの変更

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -5,7 +5,7 @@ type Props = {
 };
 
 const Layout: React.VFC<Props> = (props) => {
-  return <div className="min-h-screen ">{props.children}</div>;
+  return <div className="min-h-screen px-5 py-12  ">{props.children}</div>;
 };
 
 export default memo(Layout);
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,14 +1,8 @@
 import React from "react";
-import Header from "../../components/Header";
 import Login from "../../components/Login";
 
 const LoginPage = () => {
-  return (
-    <>
-      <Header />
-      <Login />
-    </>
-  );
+  return <Login />;
 };
 
 export default LoginPage;
