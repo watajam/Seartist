6c5dd48669049fc929eec59c29e1b86eb483825c
wataru <wata124coron@icloud.com>
2021-10-15 05:57:47 +0900

    ✨ #2 一般PF登録画面の作成

---
+++ src/pages/register/general/second.tsx
@@ -0,0 +1,12 @@
+import { memo } from "react";
+import SecondForm from "../../../components/GeneralForm/SecondForm";
+
+export const SecondPage: React.VFC = () => {
+  return (
+    <>
+      <SecondForm />
+    </>
+  );
+};
+
+export default memo(SecondPage);
