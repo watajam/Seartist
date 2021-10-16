c8ad8bc4f88c2483e721533775f0407e8b66f0d4
wataru <wata124coron@icloud.com>
2021-10-17 04:35:22 +0900

    ✨ メイン画面のデザイン作成

---
+++ src/components/Posts/index.tsx
@@ -0,0 +1,35 @@
+import React, { memo } from "react";
+import { HiUserCircle } from "react-icons/hi";
+
+export const index: React.VFC = () => {
+  return (
+    <div className="grid gap-6 ">
+      <div className="shadow rounded-2xl">
+        <header className="bg-gray-400 text-white flex  p-4 rounded-t-2xl items-center font-bold text-2xl ">
+          <HiUserCircle className="w-8 h-8" />
+          <h1 className="ml-2">WATARU</h1>
+          <time dateTime="2022-01-24" className="ml-auto">
+            2022/01/24
+          </time>
+        </header>
+        <div className="p-4">
+          <p className="text-base text-bold font-bold">
+            XXXXX開催！！ 今回は演出をパワーアップして開催します
+            お待ちしてます！
+          </p>
+          <div className="bg-orange-100 text-center h-48 mt-6">写真</div>
+          <dl className="flex flex-wrap text-base font-bold mt-4">
+            <dt className="w-3/12">開催時間</dt>
+            <dd className="w-3/4">22:00~4:00</dd>
+            <dt className="w-3/12 mt-2">開催場所</dt>
+            <dd className="mt-2">Zepp Tokyo</dd>
+            <dt className="ml-auto mt-2">♡</dt>
+            <dd className="mt-2">200</dd>
+          </dl>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default memo(index);
---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AppLayout from "../../components/Layout/AppLayout";
+import Posts from "../../components/Posts";
+
+export const PostsPage = () => {
+  return (
+    <AppLayout>
+      <Posts />
+    </AppLayout>
+  );
+};
+
+export default PostsPage;
