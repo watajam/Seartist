ab53e3cf4951767f8a0c7ae6205aa4ae7f534bb6
wataru <wata124coron@icloud.com>
2021-10-17 07:32:17 +0900

    ✨ 投稿ボタンの作成とBootmNavのデザインの変更

--- src/components/BottomNavigation/index.tsx
+++ src/components/BottomNavigation/index.tsx
@@ -6,56 +6,56 @@ import { BiSearchAlt } from "react-icons/bi";
 import { BiSearch } from "react-icons/bi";
 import { BiUserCircle } from "react-icons/bi";
 import { HiUserCircle } from "react-icons/hi";
+import { RiQuillPenLine } from "react-icons/ri";
 import { useRouter } from "next/dist/client/router";
-
-const BOTTOM_NAV = [
-  {
-    href: "/posts",
-    clickIcon: <HiHome className="w-6 h-6 text-white inline-block mb-1 " />,
-    icon: <HiOutlineHome className="w-6 h-6 text-white inline-block mb-1 " />,
-    name: "ホーム",
-  },
-  {
-    href: "/explore",
-    clickIcon: (
-      <BiSearchAlt className="w-6 h-6 text-white inline-block mb-1 " />
-    ),
-    icon: <BiSearch className="w-6 h-6 text-white inline-block mb-1 " />,
-    name: "検索",
-  },
-  {
-    href: "/profile/[userId]",
-    clickIcon: (
-      <HiUserCircle className="w-6 h-6 text-white inline-block mb-1 " />
-    ),
-    icon: <BiUserCircle className="w-6 h-6 text-white inline-block mb-1 " />,
-    name: "プロフィール",
-  },
-];
 
 export const BottomNavigation: React.VFC = () => {
   const router = useRouter();
 
   return (
-    <aside className="w-full block fixed  bottom-0 z-10 bg-orange-400 ">
-      <nav>
+    <aside>
+      <nav className="w-full fixed  bottom-0 z-10 bg-orange-400 ">
         <ul className="flex justify-around">
-          {BOTTOM_NAV.map((item, index) => {
-            return (
-              <li key={index}>
-                <Link href={item.href}>
-                  <a className="w-full  justify-center inline-block text-center pt-2 pb-1">
-                    {router.pathname === item.href ? item.clickIcon : item.icon}
-                    <span className=" block text-xs text-white">
-                      {item.name}
-                    </span>
-                  </a>
-                </Link>
-              </li>
-            );
-          })}
+          <li className="pt-2 pb-4">
+            <Link href="/posts">
+              <a className="w-full  justify-center inline-block text-center ">
+                {router.pathname === "/posts" ? (
+                  <HiHome className="w-7 h-7  text-white inline-block  " />
+                ) : (
+                  <HiOutlineHome className="w-7 h-7  text-white inline-block  " />
+                )}
+              </a>
+            </Link>
+          </li>
+          <li className="pt-2 pb-4">
+            <Link href="/profile/user">
+              <a className="w-full  justify-center inline-block text-center ">
+                {router.pathname === "/profile/user" ? (
+                  <BiSearchAlt className="w-7 h-7 text-white inline-block  " />
+                ) : (
+                  <BiSearch className="w-7 h-7 text-white inline-block  " />
+                )}
+              </a>
+            </Link>
+          </li>
+          <li className="pt-2 pb-4">
+            <Link href="/profile/[userId]">
+              <a className="w-full  justify-center inline-block text-center ">
+                {router.pathname === "/profile/[userId]" ? (
+                  <HiUserCircle className="w-7 h-7 text-white inline-block  " />
+                ) : (
+                  <BiUserCircle className="w-7 h-7 text-white inline-block  " />
+                )}
+              </a>
+            </Link>
+          </li>
         </ul>
       </nav>
+      <Link href="/posts">
+        <a className="fixed bottom-20 bg-orange-400 right-4 rounded-full p-4">
+          <RiQuillPenLine className="w-6 h-6  text-white " />
+        </a>
+      </Link>
     </aside>
   );
 };
