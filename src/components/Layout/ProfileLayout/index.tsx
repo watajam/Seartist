110a6ee80be7a0dd234a35fe499f3c53107380da
wataru <wata124coron@icloud.com>
2021-10-17 10:52:54 +0900

    ✨ ProfileLayoutコンポーネントの作成

---
+++ src/components/Layout/ProfileLayout/index.tsx
@@ -0,0 +1,19 @@
+import React, { memo } from "react";
+import { BottomNavigation } from "../../BottomNavigation";
+import Header from "../../Header";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const ProfileLayout: React.VFC<Props> = (props) => {
+  return (
+    <div className="min-h-screen">
+      <Header />
+      <BottomNavigation />
+      <div className="pt-5 mb-14">{props.children}</div>
+    </div>
+  );
+};
+
+export default memo(ProfileLayout);
