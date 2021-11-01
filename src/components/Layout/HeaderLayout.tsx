5c0aef4317cf03bcef2289cb6e38682de381d969
wataru <wata124coron@icloud.com>
2021-11-01 20:10:21 +0900

    📱 レスポンシブデザインの修正

--- src/components/Layout/HeaderLayout.tsx
+++ src/components/Layout/HeaderLayout.tsx
@@ -8,9 +8,9 @@ type Props = {
 const HeaderLayout: VFC<Props> = (props) => {
   return (
     <>
-      <div className="flex flex-col items-center  min-h-screen sm:bg-gray-100 ">
+      <div className="flex flex-col items-center  min-h-screen sm:bg-gray-100">
         <Header />
-        <main className="px-5 py-12 w-full sm:mt-20 sm:max-w-xl sm:shadow-lg sm:rounded-2xl sm:bg-white">
+        <main className="px-5 py-12 w-full sm:max-w-xl sm:bg-white md:my-20 md:max-w-2xl md:shadow-lg md:rounded-2xl ">
           {props.children}
         </main>
       </div>
