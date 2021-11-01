2f568fef5172029101e21c7ff403ac4e481e97d0
wataru <wata124coron@icloud.com>
2021-11-01 12:32:58 +0900

    📱 ログイン画面＆プロフィール登録画面のレスポンシブ

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,7 +2,7 @@ import React, { memo, VFC } from "react";
 
 const Header: VFC = () => {
   return (
-    <header className="px-4 py-3 text-4xl font-bold text-white bg-orange-300">
+    <header className="px-4 py-3 text-4xl font-bold text-white bg-orange-300 w-screen ">
       Seartist
     </header>
   );
