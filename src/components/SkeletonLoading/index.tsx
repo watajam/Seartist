ece9be8866496e29bcbbbad91f7a05623771b9fb
wataru <wata124coron@icloud.com>
2021-11-10 08:33:35 +0900

    ♻️ スケルトンローディングのリファクタリング

---
+++ src/components/SkeletonLoading/index.tsx
@@ -0,0 +1,14 @@
+import React, { VFC } from "react";
+import SkeletonLoadingItem from "./SkeletonLoadingItem";
+
+const SkeletonLoading: VFC = () => {
+  return (
+     <div className="grid gap-6  md:max-w-xl lg:max-w-2xl">
+      {[1, 2, 3, 4, 5].map((index) => {
+        return <SkeletonLoadingItem key={index} />;
+      })}
+    </div>
+  );
+};
+
+export default SkeletonLoading;
