73ae5c55fe7ea04db677a526437be25d7c17d912
wataru <wata124coron@icloud.com>
2021-11-12 00:13:46 +0900

    ✨投稿機能をアカウントによって制限をつけました

--- src/hooks/usePostCreateReactHookForm.ts
+++ src/hooks/usePostCreateReactHookForm.ts
@@ -1,7 +1,13 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useRouter } from "next/dist/client/router";
 import { useForm } from "react-hook-form";
-import { addDoc, collection, serverTimestamp } from "@firebase/firestore";
+import {
+  addDoc,
+  collection,
+  doc,
+  onSnapshot,
+  serverTimestamp,
+} from "@firebase/firestore";
 import { db } from "../../lib/firebase";
 import { FormData } from "../../types/FormData";
 import { useRecoilSetEmail } from "./useRecoilSetEmail";
@@ -33,7 +39,21 @@ export const usePostCreateReactHookForm = (url: string) => {
   >({
     mode: "onChange",
   });
+  const [user, setUser] = useState<Pick<FormData, "genre">>(null);
   const { userEmail } = useRecoilSetEmail();
+
+  // ユーザー情報取得
+  useEffect(() => {
+    if (userEmail !== null) {
+      const postsRef = doc(db, "users", userEmail.email);
+      const unsubscribe = onSnapshot(postsRef, (snapshot) => {
+        setUser({
+          genre: snapshot.data().genre,
+        });
+      });
+      return () => unsubscribe();
+    }
+  }, [userEmail]);
 
   //追加
   const postsRef =
@@ -41,26 +61,33 @@ export const usePostCreateReactHookForm = (url: string) => {
       ? collection(db, "users", userEmail.email, "posts")
       : null;
 
-  const onSubmit = useCallback(async (data: FormData) => {
-    await addDoc(postsRef, {
-      image: data.image,
-      writing: data.writing,
-      eventName: data.eventName,
-      genre: data.genre,
-      location: data.location,
-      eventLocation: data.eventLocation,
-      eventDate: data.eventDate,
-      openTime: data.openTime,
-      closeTime: data.closeTime,
-      minAmount: data.minAmount,
-      maxAmount: data.maxAmount,
-      coupon: data.coupon,
-      tickets: data.tickets,
-      timestamp: serverTimestamp(),
-      email: userEmail?.email,
-    });
-    router.push(url);
-  }, []);
+  const onSubmit = useCallback(
+    async (data: FormData) => {
+      if (user.genre !== "" && user.genre !== null) {
+        await addDoc(postsRef, {
+          image: data.image,
+          writing: data.writing,
+          eventName: data.eventName,
+          genre: data.genre,
+          location: data.location,
+          eventLocation: data.eventLocation,
+          eventDate: data.eventDate,
+          openTime: data.openTime,
+          closeTime: data.closeTime,
+          minAmount: data.minAmount,
+          maxAmount: data.maxAmount,
+          coupon: data.coupon,
+          tickets: data.tickets,
+          timestamp: serverTimestamp(),
+          email: userEmail?.email,
+        });
+      } else {
+        alert("投稿できるのはクリエイターアカウントのみです");
+      }
+      router.push(url);
+    },
+    [user]
+  );
 
   return { register, handleSubmit, errors, onSubmit, setValue };
 };
