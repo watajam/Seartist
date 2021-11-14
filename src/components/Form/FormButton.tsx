90c44023ca1f909ac470956675c895cbe07a090c
wataru <wata124coron@icloud.com>
2021-11-15 01:09:17 +0900

    ✨ 遷移元に遷移する機能を追加しました。

--- src/components/Form/FormButton.tsx
+++ src/components/Form/FormButton.tsx
@@ -1,19 +1,32 @@
 import React, { memo, VFC } from "react";
 import Link from "next/link";
+import { useBackPage } from "../../hooks/useBackPage";
 
 type Props = {
-  backButtonUrl: string;
+  backButtonUrl?: string;
   title: string;
 };
 
 const FormButton: VFC<Props> = (props) => {
+  const { backPage } = useBackPage();
   return (
     <div className="flex justify-between ">
-      <Link href={props.backButtonUrl}>
-        <a className="w-12/25   text-center inline-block py-3  text-2xl font-bold text-white rounded-xl bg-gray-400 hover:bg-gray-500">
+      {props.backButtonUrl ? (
+        <Link href={props.backButtonUrl}>
+          <a className="w-12/25   text-center inline-block py-3  text-2xl font-bold text-white rounded-xl bg-gray-400 hover:bg-gray-500">
+            戻る
+          </a>
+        </Link>
+      ) : (
+        <button
+            onClick={backPage}
+            type="button"
+          className="w-12/25   text-center inline-block py-3  text-2xl font-bold text-white rounded-xl bg-gray-400 hover:bg-gray-500"
+        >
           戻る
-        </a>
-      </Link>
+        </button>
+      )}
+
       <button className="w-12/25  py-3  text-2xl font-bold text-white bg-orange-300 border rounded-xl hover:bg-orange-400">
         {props.title}
       </button>
--- src/components/Header/PostDetailHeader.tsx
+++ src/components/Header/PostDetailHeader.tsx
@@ -1,10 +1,13 @@
 import React, { memo, VFC } from "react";
 import { IoMdArrowBack } from "react-icons/io";
+import { useBackPage } from "../../hooks/useBackPage";
 
 const PostDetailHeader: VFC = () => {
+  const {backPage} = useBackPage();
+
   return (
     <header className="px-4 py-3 text-4xl font-bold text-white bg-orange-300 fixed w-full z-10 md:max-w-xl lg:max-w-2xl">
-      <IoMdArrowBack />
+      <IoMdArrowBack onClick={backPage} />
     </header>
   );
 };
