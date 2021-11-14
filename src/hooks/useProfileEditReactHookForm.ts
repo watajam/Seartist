09114b1354dca834350d8466e24a6c5d16b4b037
wataru <wata124coron@icloud.com>
2021-11-15 01:12:39 +0900

    ✨ プロフィール編集機能の作成を行いました。

--- src/hooks/useProfileEditReactHookForm.ts
+++ src/hooks/useProfileEditReactHookForm.ts
@@ -1,21 +1,30 @@
 import { useCallback } from "react";
 import { useRouter } from "next/dist/client/router";
 import { useForm } from "react-hook-form";
-import { doc, setDoc } from "@firebase/firestore";
+import {
+  collection,
+  doc,
+  getDocs,
+  query,
+  updateDoc,
+  where,
+} from "@firebase/firestore";
 import { auth, db } from "../../lib/firebase";
 import { FormData } from "../../types/FormData";
 
-export const useProfileEditReactHookForm = (url: string) => {
+export const useProfileEditReactHookForm = () => {
   const router = useRouter();
+
   const {
     register,
     handleSubmit,
     formState: { errors },
     setValue,
+    setError,
   } = useForm<
     Pick<
       FormData,
-      "image"
+      | "image"
       | "name"
       | "userId"
       | "birthday"
@@ -32,11 +41,34 @@ export const useProfileEditReactHookForm = (url: string) => {
   });
 
   const onSubmit = useCallback(async (data: FormData) => {
-    await setDoc(doc(db, "posts", auth.currentUser.email), {
-      image: data.image,
-      writing: data.writing,
-    });
-    router.push(url);
+    const q = query(
+      collection(db, "users"),
+      where("userId", "==", data.userId),
+      where("email", "==", auth.currentUser?.email)
+    );
+
+    const currentUser = await getDocs(q);
+    if (currentUser.docs.length !== 1) {
+      setError("userId", {
+        type: "validate",
+        message: "このユーザーIDは既に使用されています",
+      });
+    } else {
+      await updateDoc(doc(db, "users", auth.currentUser.email), {
+        image: data.image,
+        name: data.name,
+        userId: data.userId,
+        genre: data.genre ? data.genre : "",
+        location: data.location,
+        birthday: data.birthday,
+        writing: data.writing,
+        twitterUrl: data.twitterUrl,
+        instagramUrl: data.instagramUrl,
+        homepageUrl: data.homepageUrl,
+        otherUrl: data.otherUrl,
+      });
+      router.back();
+    }
   }, []);
 
   return { register, handleSubmit, errors, onSubmit, setValue };
