3fa9260db26b16839d006aa73b541cefbf891fcd
wataru <wata124coron@icloud.com>
2021-11-24 01:35:03 +0900

    🏷️ 新規にタイプを追加しました

---
+++ types/PostData.ts
@@ -0,0 +1,12 @@
+export type PostData = {
+  genre: string;
+  eventName: string;
+  eventLocation: string;
+  eventDate: string;
+  openTime: string;
+  closeTime: string;
+  id?: string;
+  image?: string;
+  writing?: string;
+  email?: string;
+};
---
+++ types/PostDetailData.ts
@@ -0,0 +1,17 @@
+export type PostDetailData = {
+  genre: string;
+  location: string;
+  eventName: string;
+  eventLocation: string;
+  eventDate: string;
+  openTime: string;
+  closeTime: string;
+  id?: string;
+  image?: string;
+  writing?: string;
+  minAmount?: string;
+  maxAmount?: string;
+  tickets?: string;
+  coupon?: string;
+  email?: string;
+};
