38e81737fffa4a9a74ed61d6f9c1ca285f30f18a
wataru <wata124coron@icloud.com>
2021-12-02 02:48:39 +0900

    ✨ プロフィール画面のHeaderに戻る機能を追加

--- src/components/Header/ProfileHeader.tsx
+++ src/components/Header/ProfileHeader.tsx
@@ -1,13 +1,17 @@
 import React, { memo, VFC } from 'react';
 import { AiOutlineLogout } from 'react-icons/ai';
 import { useAuthLogout } from '../../../FireBase/Authentication/useAuthLogout';
+import { useBackPage } from '../../hooks/useBackPage';
+import { IoMdArrowBack } from 'react-icons/io';
 
 const ProfileHeader: VFC = () => {
   const { logout } = useAuthLogout();
 
+  const { backPage } = useBackPage();
+
   return (
     <header className="px-4 py-3 text-4xl font-bold text-white bg-orange-300 fixed w-full bg-opacity-95 z-10 flex justify-between  md:max-w-xl lg:max-w-2xl">
-      Seartist
+      <IoMdArrowBack onClick={backPage} />
       <button onClick={logout}>
         <AiOutlineLogout />
       </button>
