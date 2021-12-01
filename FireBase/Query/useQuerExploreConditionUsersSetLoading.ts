2cd949ed8798e52ff5c2ea8ff2b98d5584a8144b
wataru <wata124coron@icloud.com>
2021-12-02 02:41:55 +0900

    ✨ 条件検索機能の作成を行った。

---
+++ FireBase/Query/useQuerExploreConditionUsersSetLoading.ts
@@ -0,0 +1,34 @@
+import { collection, getDocs, query, where } from '@firebase/firestore';
+import { useEffect, useState } from 'react';
+import { db } from '../../lib/firebase';
+import { UserData } from '../../types/UserData';
+
+export const useQuerExploreConditionUsersSetLoading = (posts) => {
+  const [users, setUsers] = useState<Pick<UserData, 'userId' | 'name' | 'image'>[]>([]);
+
+  useEffect(() => {
+    if (posts === null) {
+      return;
+    }
+
+    const usersRef = collection(db, 'users');
+
+    posts?.forEach(async (post) => {
+      const q = query(usersRef, where('email', '==', post.email));
+
+      const querySnap = await getDocs(q);
+
+      if (querySnap.empty) {
+        return;
+      } else {
+        querySnap.forEach((user) => {
+          setUsers((prev) => {
+            return [...prev, { userId: user.data().userId, name: user.data().name, image: user.data().image }];
+          });
+        });
+      }
+    });
+  }, [posts]);
+
+  return { users };
+};
