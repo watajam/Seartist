8e35c7689a558e67d07206063d83d4cb2746de1c
wataru <wata124coron@icloud.com>
2021-12-05 20:51:59 +0900

    🎨 使用していないコードを削除

--- src/components/Form/FormList/UsersExploreFormList.tsx
+++ src/components/Form/FormList/UsersExploreFormList.tsx
@@ -1,32 +1,16 @@
-import router from 'next/router';
-import React, { memo, useState, VFC } from 'react';
+import React, { memo, VFC } from 'react';
 import { SearchBox, Configure } from 'react-instantsearch-dom';
 import { InstantSearch } from 'react-instantsearch-dom';
 import { indexName, searchClient } from '../../../../lib/searchClient';
 import { UserExplore } from '../../Explore/UsersExplore';
 
 const UsersExploreFormList: VFC = () => {
-  const [querystate, setQueryState] = useState();
-
   return (
     <>
-      <InstantSearch
-        indexName={indexName}
-        searchClient={searchClient}
-        onSearchStateChange={(state) => {
-          setQueryState(state.query);
-        }}
-      >
+      <InstantSearch indexName={indexName} searchClient={searchClient}>
         <Configure hitsPerPage={5} />
 
-        <SearchBox
-          onSubmit={(event) => {
-            event.preventDefault();
-            router.push(`/explore/users?userquery=${querystate}`);
-          }}
-          translations={{ placeholder: '氏名 ・ ユーザーID ・ ジャンル ・ 所在地検索' }}
-          className="mt-2"
-        />
+        <SearchBox translations={{ placeholder: '氏名 ・ ユーザーID ・ ジャンル ・ 所在地' }} className="mt-2" />
 
         <UserExplore />
       </InstantSearch>
