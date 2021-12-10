a384cc5ac30c22c4fbc9db2d324d1f4a0fea86ee
wataru <wata124coron@icloud.com>
2021-12-11 01:09:27 +0900

    ♻️ Profileページのリファクタリング

--- src/pages/profile/[id]/index.tsx
+++ src/pages/profile/[id]/index.tsx
@@ -1,79 +1,43 @@
 import { GetServerSideProps, NextPage } from 'next';
-import { FormData } from '../../../../types/FormData';
 import ProfileLayout from '../../../components/Layout/ProfileLayout';
 import Profile from '../../../components/Profile';
 import db from '../../../../lib/nodeApp';
+import { UserData } from '../../../../types/UserData';
+import { PostData } from '../../../../types/PostData';
 
 type Props = {
-  profile: Pick<
-    FormData,
-    | 'image'
-    | 'name'
-    | 'userId'
-    | 'genre'
-    | 'location'
-    | 'birthday'
-    | 'writing'
-    | 'image'
-    | 'twitterUrl'
-    | 'instagramUrl'
-    | 'homepageUrl'
-    | 'otherUrl'
-    | 'email'
-  >[];
-  posts: Pick<
-    FormData,
-    'id' | 'image' | 'writing' | 'eventName' | 'genre' | 'eventLocation' | 'eventDate' | 'openTime' | 'closeTime'
-  >[];
+  userInfo: UserData[];
+  posts: Omit<PostData, 'emial'>[];
 };
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   const { id } = ctx.query;
-  // const db = admin.firestore()
-  const profile: Pick<
-    FormData,
-    | 'image'
-    | 'name'
-    | 'userId'
-    | 'genre'
-    | 'location'
-    | 'birthday'
-    | 'writing'
-    | 'image'
-    | 'twitterUrl'
-    | 'instagramUrl'
-    | 'homepageUrl'
-    | 'otherUrl'
-    | 'email'
-  >[] = [];
-  const posts: Pick<
-    FormData,
-    'id' | 'image' | 'writing' | 'eventName' | 'genre' | 'eventLocation' | 'eventDate' | 'openTime' | 'closeTime'
-  >[] = [];
+  const userInfo: UserData[] = [];
+  const posts: Omit<PostData, 'emial'>[] = [];
 
   try {
     const userRef = db.collection('users');
-    const userSnapshot = await userRef.where('userId', '==', id).get();
+    const userSnap = await userRef.where('userId', '==', id).get();
 
-    userSnapshot.forEach((doc) => {
-      const data = doc.data();
-      profile.push({
-        image: data.image ? data.image : '',
-        name: data?.name,
-        userId: data?.userId,
-        genre: data?.genre ? data?.genre : '',
-        location: data?.location ? data?.location : '',
-        birthday: data?.birthday ? data?.birthday : '',
-        writing: data?.writing ? data?.writing : '',
-        twitterUrl: data?.twitterUrl ? data?.twitterUrl : '',
-        instagramUrl: data?.instagramUrl ? data?.instagramUrl : '',
-        homepageUrl: data?.homepageUrl ? data?.homepageUrl : '',
-        otherUrl: data?.otherUrl ? data?.otherUrl : '',
-        email: data?.email,
-      });
-    });
     try {
-      const postsRef = db.collection(`users`).doc(`${profile[0].email}`).collection('posts');
+      userSnap.forEach((doc) => {
+        const data = doc.data();
+        userInfo.push({
+          image: data.image ? data.image : '',
+          name: data?.name,
+          userId: data?.userId,
+          genre: data?.genre ? data?.genre : '',
+          location: data?.location ? data?.location : '',
+          birthday: data?.birthday ? data?.birthday : '',
+          writing: data?.writing ? data?.writing : '',
+          twitterUrl: data?.twitterUrl ? data?.twitterUrl : '',
+          instagramUrl: data?.instagramUrl ? data?.instagramUrl : '',
+          homepageUrl: data?.homepageUrl ? data?.homepageUrl : '',
+          otherUrl: data?.otherUrl ? data?.otherUrl : '',
+          email: data?.email,
+        });
+      });
+      const postsRef = db.collection(`users`).doc(`${userInfo[0].email}`).collection('posts');
       const postsSnapshot = await postsRef.orderBy('timestamp', 'desc').get();
       postsSnapshot.forEach((doc) => {
         const postsData = JSON.parse(JSON.stringify(doc.data()));
@@ -88,18 +52,18 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 
   return {
     props: {
-      profile,
+      userInfo,
       posts,
     },
   };
 };
 
 const ProfilePage: NextPage<Props> = (props) => {
-  const { profile, posts } = props;
+  const { userInfo, posts } = props;
 
   return (
     <ProfileLayout>
-      <Profile user={profile[0]} postsLength={posts.length} />
+      <Profile user={userInfo[0]} postsLength={posts.length} />
     </ProfileLayout>
   );
 };
