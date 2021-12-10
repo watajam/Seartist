87585793542f41ecfc649e8581743aef9c3fe59d
wataru <wata124coron@icloud.com>
2021-12-10 19:12:32 +0900

    ♻️ ユーザー情報の初期化処理のリファクタリグ

--- src/components/Selection/index.tsx
+++ src/components/Selection/index.tsx
@@ -1,36 +1,16 @@
-import React, { memo, useCallback, VFC } from 'react';
+import React, { memo, VFC } from 'react';
 import Link from 'next/link';
-import { doc, setDoc } from '@firebase/firestore';
-import { db } from '../../../lib/firebase';
-import { useRecoilSetEmail } from '../../hooks/useRecoilSetEmail';
+import { useInsertUserInfo } from '../../../FireBase/Mutation/Insert/useInsertUserInfo';
 
 const Selection: VFC = () => {
-  const { userEmail } = useRecoilSetEmail();
-
-  const handleSetUserEmail = useCallback(() => {
-    if (userEmail !== null) {
-      setDoc(doc(db, 'users', userEmail.email), {
-        image: '',
-        name: '',
-        userId: '',
-        genre: '',
-        location: '',
-        birthday: '',
-        writing: '',
-        twitterUrl: '',
-        instagramUrl: '',
-        homepageUrl: '',
-        otherUrl: '',
-      });
-    }
-  }, [userEmail]);
+  const { handleSetUserInfo } = useInsertUserInfo();
 
   return (
     <>
       <h1 className="text-xl font-bold text-center text-gray-400 underline mt-8">該当している方を選択してください</h1>
       <Link href="/listener">
         <a
-          onClick={handleSetUserEmail}
+          onClick={handleSetUserInfo}
           className="w-full block  py-3 text-center mt-40 text-2xl font-bold text-white bg-orange-300 border rounded-xl hover:bg-orange-400"
         >
           一般の方はこちら
@@ -39,7 +19,7 @@ const Selection: VFC = () => {
       <hr className="mt-14 border-gray-400" />
       <Link href="/creator">
         <a
-          onClick={handleSetUserEmail}
+          onClick={handleSetUserInfo}
           className="w-full block  py-3 text-center mt-14 text-2xl font-bold text-white bg-orange-300 border rounded-xl hover:bg-orange-400"
         >
           アーティストや
