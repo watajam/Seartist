29586669fd1bf8284b42c86770c190e8ea11e6fd
wataru <wata124coron@icloud.com>
2021-12-16 13:35:20 +0900

    ♻️ プロフィールのリファクタリング

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -2,18 +2,15 @@ import React, { memo, useCallback, useState, VFC } from 'react';
 import { UserData } from '../../../types/UserData';
 import ProfileUser from './ProfileUser';
 import ProfilePost from './ProfilePost';
+import ProfileLikePost from './ProfileLikePost';
 import ProfileTab from './ProfileTab';
 import { useQueryUserEmailCheck } from '../../../FireBase/Query/User/useQueryUserEmailCheck';
-import { useQueryProfilePosts } from '../../../FireBase/Query/Profile/useQueryProfilePosts';
-import { useQueryProfileUserInfo } from '../../../FireBase/Query/Profile/useQueryProfileUserInfo';
 
 type Props = {
   user: UserData;
 };
 
 const Profile: VFC<Props> = (props) => {
-  const { user, userLoading } = useQueryProfileUserInfo();
-  const { posts, postsLoading } = useQueryProfilePosts(user);
   useQueryUserEmailCheck();
   const [chengePage, setChengePage] = useState(true);
 
@@ -35,9 +32,9 @@ const Profile: VFC<Props> = (props) => {
         <ProfileTab user={props.user} handleChengePage={handleChengePage} chengePage={chengePage} />
       </div>
 
-      {/* プロフィール投稿 */}
+      {/* プロフィール投稿といいねした投稿 */}
       <div className="px-5 mt-4 grid gap-6  md:max-w-xl lg:max-w-2xl">
-        <ProfilePost posts={posts} user={user} userLoading={userLoading} postsLoading={postsLoading} />
+        {props.user.genre === '' ? <ProfileLikePost /> : chengePage === true ? <ProfilePost /> : <ProfileLikePost />}
       </div>
     </>
   );
