1f9c6997b1756c1a68e89b96d7bf04538194d2ec
wataru <wata124coron@icloud.com>
2021-12-18 10:08:14 +0900

    ✨ フォロー機能の作成

--- src/components/Profile/ProfileUser.tsx
+++ src/components/Profile/ProfileUser.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import React, { memo, VFC } from 'react';
+import React, { memo, useCallback, useEffect, useState, VFC } from 'react';
 import { BsInstagram } from 'react-icons/bs';
 import { FiTwitter } from 'react-icons/fi';
 import { IoHomeOutline } from 'react-icons/io5';
@@ -10,12 +10,107 @@ import { IoLocationSharp } from 'react-icons/io5';
 import { UserData } from '../../../types/UserData';
 import { useRecoilSetEmail } from '../../hooks/useRecoilSetEmail';
 import ProfileEditSkeletonLoadingItem from '../SkeletonLoading/ProfileEditSkeletonLoadingItem';
+import { auth, db } from '../../../lib/firebase';
+import {
+  collection,
+  doc,
+  getDocs,
+  increment,
+  onSnapshot,
+  query,
+  serverTimestamp,
+  where,
+  writeBatch,
+} from 'firebase/firestore';
+import { useRouter } from 'next/router';
+import { onAuthStateChanged } from 'firebase/auth';
 
 type Props = {
   user: UserData;
 };
 const ProfileUser: VFC<Props> = (props) => {
   const { userEmail } = useRecoilSetEmail();
+  const [userFollowingInfo, setUserFollowingInfo] = useState('');
+  const router = useRouter();
+
+  const handleFollowUs = useCallback(async (email) => {
+    const batch = writeBatch(db);
+    //自分の情報
+    const authUserRef = doc(db, 'users', auth.currentUser?.email);
+    const authFollowingRef = doc(authUserRef, 'following', email);
+    const authFollowersColRef = collection(authUserRef, 'followers');
+    const authFollowersDocRef = doc(authUserRef, 'followers', email);
+
+    //フォローした人の情報
+    const otherUsers = doc(db, 'users', email);
+    const otherFollowersRef = doc(otherUsers, 'followers', auth.currentUser?.email);
+
+    //自分のフォロワーにフォローした人が存在するか確認
+    const q = query(authFollowersColRef, where('email', '==', email));
+    const likePostsquery = await getDocs(q);
+
+    if (likePostsquery.docs.length === 1) {
+      //フォローした人が自分のフォロワーにいた場合
+      batch.set(authFollowingRef, {
+        email,
+        createTime: serverTimestamp(),
+        following: true,
+      });
+      batch.set(otherFollowersRef, {
+        email: auth.currentUser?.email,
+        createTime: serverTimestamp(),
+        following: true,
+      });
+      batch.update(authFollowersDocRef, { following: true });
+      batch.update(authUserRef, { followUsersCount: increment(1) });
+      batch.update(otherUsers, { followerUsersCount: increment(1) });
+      await batch.commit();
+    } else {
+      //自分のフォローコレクションにフォローした人の情報を格納
+      batch.set(authFollowingRef, {
+        email,
+        createTime: serverTimestamp(),
+        following: true,
+      });
+
+      //フォローした人のフォロワーコレクションに自分の情報を格納
+      batch.set(otherFollowersRef, {
+        email: auth.currentUser?.email,
+        createTime: serverTimestamp(),
+        following: false,
+      });
+      batch.update(authUserRef, { followUsersCount: increment(1) });
+      batch.update(otherUsers, { followerUsersCount: increment(1) });
+      await batch.commit();
+    }
+  }, []);
+
+  //自分のフォローコレクションに表示されているプロフィールユーザーの情報が既にあるか確認
+  useEffect(() => {
+    const userFollowingCheck = async () => {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          if (user.email !== props.user?.email) {
+            const authUserRef = doc(db, 'users', user.email);
+            const q = query(collection(authUserRef, 'following'), where('email', '==', props.user?.email));
+            const unSub = onSnapshot(q, (querySnap) => {
+              if (querySnap.docs.length === 0) {
+                setUserFollowingInfo('フォローする');
+                return;
+              } else {
+                setUserFollowingInfo('フォロー中');
+              }
+            });
+            return () => unSub();
+          }
+        } else {
+          router.push('/login');
+        }
+      });
+    };
+    userFollowingCheck();
+  }, [router.query?.id]);
+
 
   return (
     <>
@@ -138,8 +233,22 @@ const ProfileUser: VFC<Props> = (props) => {
         <Link href="/profile/editprofile">
           <a className="bg-orange-400 text-white text-center mt-6 p-1 block">プロフィール編集</a>
         </Link>
+      ) : userFollowingInfo === '' ? (
+        <ProfileEditSkeletonLoadingItem />
+      ) : userFollowingInfo === 'フォローする' ? (
+        <button
+          onClick={() => handleFollowUs(props.user?.email)}
+          className="bg-orange-400 text-white text-center mt-6 p-1 w-full"
+        >
+          フォローする
+        </button>
       ) : (
-        <div className="bg-orange-400 text-white text-center mt-6 p-1">フォローする</div>
+        <button
+          // onClick={() => handleFollowUs(props.user?.email)}
+          className="bg-gray-400 text-white text-center mt-6 p-1 w-full"
+        >
+          フォロー中
+        </button>
       )}
     </>
   );
