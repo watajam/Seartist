3cb74aff1bf339905582da37f1466ce2053c3a90
watajam <wata124coron@icloud.com>
2022-01-17 17:46:03 +0900

    🎨 next-seo.configファイルの作成し適応

---
+++ next-seo.config.ts
@@ -0,0 +1,27 @@
+export default {
+  title: 'Seartist',
+  description:
+    'Seartistは、アーティストや主催者がイベント情報を告知し、沢山の方々にイベント情報を知ってもらう為のアプリです。',
+  canonical: 'https://seartist.vercel.app/',
+  noindex: true,
+  openGraph: {
+    url: 'https://seartist.vercel.app',
+    title: 'Seartist',
+    description:
+      'Seartistは、アーティストや主催者がイベント情報を告知し、沢山の方々にイベント情報を知ってもらう為のアプリです。',
+    images: [
+      {
+        url: 'https://light.dotup.org/uploda/light.dotup.org14180.png_ngKbwCYY3GZDZA6qNGPh/light.dotup.org14180.png',
+        width: 1200,
+        height: 630,
+        alt: 'Seartsitのプロフィール画面の画像',
+      },
+    ],
+    site_name: 'Seartist',
+  },
+  twitter: {
+    handle: '@seartist',
+    site: '@seartist',
+    cardType: 'summary_large_image',
+  },
+};
