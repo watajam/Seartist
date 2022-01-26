107f0d67f57274ff717f1eb674b98a96d827f4af
watajam <wata124coron@icloud.com>
2022-01-27 00:11:23 +0900

    ✨すでにログインしていた場合、自動的にホーム画面に遷移

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,9 +1,12 @@
 import HeaderLayout from '../../components/Layout/HeaderLayout';
 import LoginForm from '../../components/Form/LoginForm';
 import { NextPage } from 'next';
+import { useAuthAutoLogin } from '../../../FireBase/Authentication/uesAuthAutoLogin';
 
 //ログイン&新規登録画面
 const Login: NextPage = () => {
+  useAuthAutoLogin();
+
   return (
     <HeaderLayout>
       <LoginForm />
